553927d459435638200d4814926c499902abd953
KananValizada <kenan.veli@yahoo.com>
2023-01-31 22:00:44 +0400

    HeroBanner component design done

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-export { Footer, Navbar };
+import Exercises from "./Exercises";
+import HeroBanner from "./HeroBanner";
+import SearchExercises from "./SearchExercises";
+export { Footer, Navbar, Exercises, HeroBanner, SearchExercises };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Box } from "@mui/material";
+import { Exercises, HeroBanner, SearchExercises } from "../components";
 
 const Home = () => {
   return (
     <Box>
       <HeroBanner />
       <SearchExercises />
+      <Exercises />
     </Box>
   );
 };
